853c2af1d5ae9da0d15a66995a5f6665de82302d
ChuzaWick420 <zaiddadev@gmail.com>
2023-11-16 16:59:21 +0500

    Created main card
    - Increased size of #root to full viewport
    - Created main Card
    - Centered the main card
    - Divided the card into 2 sections

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,35 @@
 // import viteLogo from '/vite.svg'
 import './App.css'
 
+function Details() {
+	return (
+		<div className="details">
+
+		</div>
+	);
+}
+
+function Overview() {
+	return (
+		<div className="overview">
+
+		</div>
+	);
+}
+
+function MainCard () {
+	return (
+		<div className="main_card">
+			<Overview />
+			<Details />
+		</div>
+	);
+}
+
 function App() {
-  
+	return (
+		<MainCard />
+	);
 }
 
 export default App
