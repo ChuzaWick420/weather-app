124b61c7fbd684f376eb6c4e4f16562827a60a02
ChuzaWick420 <zaiddadev@gmail.com>
2023-11-28 18:30:45 +0500

    Improved theme and uv_index guage

--- src/components/highlights.jsx
+++ src/components/highlights.jsx
@@ -10,18 +10,20 @@ export function Highlights() {
 
 			<div className="panel_list">
 				<div className="panel">
-					<p className="stat_name">UV Index</p>
 					<div>
-						<div className="guage_secondary">
-							<span>50%</span>
+						<p className="stat_name">UV Index</p>
+						<div className="uv_container">
+							<div className="guage_secondary">
+								<span>50%</span>
+							</div>
+							<div className="fill_bar"></div>
 						</div>
-						<div className="fill_bar"></div>
 					</div>
 				</div>
 
 				<div className="panel">
-					<p className="stat_name">Wind Speed</p>
 					<div className="content_wrapper">
+						<p className="stat_name">Wind Speed</p>
 						<p>
 							<span className="value">72</span>
 							<span className="unit">km/h</span>
@@ -34,8 +36,8 @@ export function Highlights() {
 				</div>
 
 				<div className="panel">
-					<p className="stat_name">Sunrise and Suset</p>
 					<div className="content_wrapper">
+						<p className="stat_name">Sunrise and Suset</p>
 						<div className="sun">
 							<img src="https://cdn1.iconfinder.com/data/icons/weather-from-clouds-line/64/cloud-element-weather-sunrise-sun-up-rise-512.png" />
 							<div className="sun_rise">
